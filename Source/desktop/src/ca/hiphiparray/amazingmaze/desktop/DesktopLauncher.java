428db9987e908bc3c45ec9ea9fd5d1a4c1447532
Vincent Macri <TheVirtualMachine@users.noreply.github.com>
2017-05-10 23:24:11 -0400

    Add fullscreen support

--- Source/desktop/src/ca/hiphiparray/amazingmaze/desktop/DesktopLauncher.java
+++ Source/desktop/src/ca/hiphiparray/amazingmaze/desktop/DesktopLauncher.java
@@ -1,12 +1,20 @@
 package ca.hiphiparray.amazingmaze.desktop;
 
+import com.badlogic.gdx.Graphics.DisplayMode;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
 import ca.hiphiparray.amazingmaze.AmazingMazeGame;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
+	public static void main(String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+		DisplayMode displayMode = LwjglApplicationConfiguration.getDesktopDisplayMode();
+		config.setFromDisplayMode(displayMode);
+		config.title = "Amazing Maze";
+		config.vSyncEnabled = true;
+
 		new LwjglApplication(new AmazingMazeGame(), config);
 	}
 }
