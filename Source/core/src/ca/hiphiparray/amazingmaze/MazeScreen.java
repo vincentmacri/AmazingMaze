af6473d382a5872a017adb47d953588c9c6be92c
TheVirtualMachine <hiphiparray@vincemacri.ca>
2017-05-17 21:28:35 -0400

    Fix green fish transparency

--- Source/core/src/ca/hiphiparray/amazingmaze/MazeScreen.java
+++ Source/core/src/ca/hiphiparray/amazingmaze/MazeScreen.java
@@ -95,8 +95,8 @@ public class MazeScreen implements Screen, InputProcessor {
 		viewport = new FitViewport(this.mapWidth, this.mapHeight, camera);
 
 		// TODO: Use current level from settings as seed.
-		MapFactory m = new MapFactory(game, 1, this.mapWidth, this.mapHeight, TILE_SIZE);
-		map = m.getMap();
+		MapFactory factory = new MapFactory(game, 1, this.mapWidth, this.mapHeight, TILE_SIZE);
+		map = factory.getMap();
 		createBoundingBoxes();
 
 		mapRenderer = new OrthogonalTiledMapRenderer(map, MAP_SCALE, game.batch);
@@ -268,25 +268,21 @@ public class MazeScreen implements Screen, InputProcessor {
 
 	@Override
 	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean touchDragged(int screenX, int screenY, int pointer) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean mouseMoved(int screenX, int screenY) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean scrolled(int amount) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
